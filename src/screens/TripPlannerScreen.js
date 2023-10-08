6f0a94621381f5e27425c57c677f4f85f68e6541
surial <sureshmsfe@gmail.com>
2023-10-07 21:50:34 -0400

    connected

--- src/screens/TripPlannerScreen.js
+++ src/screens/TripPlannerScreen.js
@@ -19,11 +19,9 @@ function TripPlannerScreen() {
     // Define an async function to fetch data from the API
     async function fetchData() {
       try {
-        const response = await axios.get(
-          "172.31.74.216"
-        );
+        const response = await axios.get("http://127.0.0.1:8000/routes/");
 
-        // Set the data in the state
+        // Assuming the response is an array of objects, adjust as needed
         setData(response.data);
       } catch (error) {
         // Handle errors
@@ -36,40 +34,49 @@ function TripPlannerScreen() {
   }, []); // Empty dependency array means this effect runs once when the component mounts
 
   return (
-    <div style={backgroundStyle}>
-      <h2>New Jersey Trip Planner</h2>
-      <p>An AI-Generated Trip Planner catered to you and your needs :)</p>
+      <div style={backgroundStyle}>
+        <h2>New Jersey Trip Planner</h2>
+        <p>An AI-Generated Trip Planner catered to you and your needs :)</p>
 
-      <div>
-        <label htmlFor="origin">Origin:</label>
-        <input
-          type="text"
-          id="origin"
-          value={origin}
-          onChange={(e) => setOrigin(e.target.value)}
-        />
+        <div>
+          <label htmlFor="origin">Origin:</label>
+          <input
+              type="text"
+              id="origin"
+              value={origin}
+              onChange={(e) => setOrigin(e.target.value)}
+          />
+        </div>
+
+        <div>
+          <label htmlFor="destination">Destination:</label>
+          <input
+              type="text"
+              id="destination"
+              value={destination}
+              onChange={(e) => setDestination(e.target.value)}
+          />
+        </div>
+
+        {error ? (
+            <p>Error: {error.message}</p>
+        ) : (
+            <div>
+              <h3>API Data:</h3>
+              {Array.isArray(data) && data.length > 0 ? (
+                  <ul>
+                    {data.map((item) => (
+                        <li key={item.id}>
+                          Origin: {item.origin}, Destination: {item.destination}
+                        </li>
+                    ))}
+                  </ul>
+              ) : (
+                  <p>No data available</p>
+              )}
+            </div>
+        )}
       </div>
-
-      <div>
-        <label htmlFor="destination">Destination:</label>
-        <input
-          type="text"
-          id="destination"
-          value={destination}
-          onChange={(e) => setDestination(e.target.value)}
-        />
-      </div>
-
-      {error ? (
-        <p>Error: {error.message}</p>
-      ) : (
-        <ul>
-          {data.map((item) => (
-            <li key={item.id}>{item.name}</li>
-          ))}
-        </ul>
-      )}
-    </div>
   );
 }
 
